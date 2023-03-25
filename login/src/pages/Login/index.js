d3b7b7bb77e35c953eb83411e6e3ffd2c2b39964
andmatsou <andmatsou@gmail.com>
2023-03-25 20:33:55 -0300

    Adicionamos o formulario login

--- login/src/pages/Login/index.js
+++ login/src/pages/Login/index.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 
+export const Login = () =>{
 
-export const Login = () => {
-  return(
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
-}
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+
+    const valorInput = e => setUser({...user, [e.target.name]: e.target.value});
+
+    const loginSubmit = async e => {
+        e.preventDefault();
+        console.log(user.password);
+    }
+
+    return(
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={loginSubmit}>
+                <label>Usuário: </label>
+                <input type="text" name="email" placeholder="Digite o e-mail" onChange={valorInput} /><br /><br />
+                
+                <label>Senha: </label>
+                <input type="password" name="password" placeholder="Digite a senha" onChange={valorInput} /><br /><br />
+
+                <button type="submit">Acessar</button>
+            </form>
+        </div>
+    );
+};
