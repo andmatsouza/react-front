03b8f405a1206eade938795aa53b63270113001d
andmatsou <andmatsou@gmail.com>
2023-03-26 15:21:13 -0300

    Adicionado msg sucesso, erro e carrengando e redirecionando apos login

--- login/src/App.js
+++ login/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route,} from "react-router-dom";
 
 import { Login } from "./pages/Login";
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />          
+          <Route exact path="/" element={<Login />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />          
         </Routes>
       </BrowserRouter>
     </div>
--- login/src/pages/Login/index.js
+++ login/src/pages/Login/index.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 
+//substituiu a useHistory
+import { Link, useNavigate, useLocation } from "react-router-dom";
+
 import  api from '../../config/configApi';
 
 export const Login = () =>{
 
+    const navegate = useNavigate();
+
     const [user, setUser] = useState({
         email: '',
         password: ''
+    });
+
+    const [status, setStatus] = useState({
+        type: '',
+        mensagem: '',
+        loading: false,
     });
 
     const valorInput = e => setUser({...user, [e.target.name]: e.target.value});
@@ -14,6 +25,9 @@ export const Login = () =>{
     const loginSubmit = async e => {
         e.preventDefault();
        // console.log(user.password);
+       setStatus({
+        loading: true
+       });
 
        const headers = {
         'Content-Type': 'application/json'
@@ -21,12 +35,28 @@ export const Login = () =>{
 
        await api.post("/login", user, {headers})
        .then((response) => {
-        console.log(response)
+        //console.log(response)
+        setStatus({
+            /*type: 'success',
+            mensagem: response.data.mensagem,*/
+            loading: false
+        });
+        return navegate("/dashboard");
        }).catch((err) => {
         if(err.response){
-          console.log(err.response)
+          //console.log(err.response)
+          setStatus({
+            type: 'error',
+            mensagem: err.response.data.mensagem,
+            loading: false
+        });
         }else{
-          console.log("Erro: tente mais tarde!");
+          //console.log("Erro: tente mais tarde!");
+          setStatus({
+            type: 'error',
+            mensagem: "Erro: tente mais tarde!",
+            loading: false
+        });
         }
         
        })
@@ -35,6 +65,9 @@ export const Login = () =>{
     return(
         <div>
             <h1>Login</h1>
+            {status.type === 'error' ? <p>{status.mensagem}</p> : ""}
+            {status.type === 'success' ? <p>{status.mensagem}</p> : ""}
+            {status.loading ? <p>Validando...</p> : ""}
             <form onSubmit={loginSubmit}>
                 <label>Usuário: </label>
                 <input type="text" name="email" placeholder="Digite o e-mail" onChange={valorInput} /><br /><br />
@@ -42,7 +75,8 @@ export const Login = () =>{
                 <label>Senha: </label>
                 <input type="password" name="password" placeholder="Digite a senha" onChange={valorInput} /><br /><br />
 
-                <button type="submit">Acessar</button>
+                
+                {status.loading ? <button type="submit" disabled>Acessando...</button> : <button type="submit">Acessar</button>}
             </form>
         </div>
     );
