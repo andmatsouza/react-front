fb30debb13684dee1dc5c05ba10a54a071a637b6
andmatsou <andmatsou@gmail.com>
2023-03-28 20:06:03 -0300

    Adicionado listar usuarios e refatorado dashboard

--- login/src/pages/Dashboard/index.js
+++ login/src/pages/Dashboard/index.js
@@ -1,18 +1,21 @@
 import React, {useContext} from 'react';
-//import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { Context} from '../../Context/AuthContext';
 
 export const Dashboard = () => {
-  const token = localStorage.getItem('token');
+ 
 
   const { authenticated, handleLogout } = useContext(Context);
   console.log("Situação do usuário na página dashboard: " + authenticated);
 
   return(
     <div>
+
+      <Link to="/dashboard">Dashboard</Link><br />
+      <Link to="/users">Usuários</Link><br />
       <h1>Dashboard</h1>
-      <p>Token: {token}</p>
+      
       <button type='button' onClick={handleLogout}>Sair</button>
     </div>
   );
--- login/src/pages/Users/index.js
+++ login/src/pages/Users/index.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import api from '../../config/configApi';
 
 export const Users = () => {
+
+  const[data, setData] = useState([]);
+  const[status, setStatus] = useState({
+    type: '',
+    mensagem: ''
+  })
+
+  const getUsers = async () => {
+    const headers = {
+      'headers': {
+          'Authorization': "Bearer " + localStorage.getItem('token')          
+      }
+    }
+    await api.get("/users", headers)
+    .then((response) => {      
+      setData(response.data.users);
+    }).catch((err) => {
+      if(err.response){
+        setStatus({
+          type: 'error',
+          mensagem: err.response.data.mensagem
+        });      
+      }else{        
+        setStatus({
+          type: 'error',
+          mensagem: "Erro: Tente mais tarde!"
+        })  
+      }
+    })
+
+  }  
+
+  useEffect(() => {
+    getUsers();
+  }, []);
   return(
     <>
+
+      <Link to="/dashboard">Dashboard</Link><br />
+      <Link to="/users">Usuários</Link><br />
       <h1>Listar Usuários</h1>
+
+      {status.type === 'error' ? <p>{status.mensagem}</p> : ""}
+
+      {data.map(user => (
+        <div key={user.id}>
+          <span>{user.id}</span><br />
+          <span>{user.name}</span><br />
+          <span>{user.email}</span><br /><hr />
+        </div>
+      ))}
     </>
   );
 }
