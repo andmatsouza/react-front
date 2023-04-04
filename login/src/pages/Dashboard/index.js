8f4264553029920e1788da94b03289bfb04d1505
andmatsou <andmatsou@gmail.com>
2023-04-03 21:58:01 -0300

    Adicionado o layout dashboard

--- login/src/pages/Dashboard/index.js
+++ login/src/pages/Dashboard/index.js
@@ -9,7 +9,35 @@ export const Dashboard = () => {
       <Navbar/>
       <div className="content">
           <Sidebar active="dashboard" />
-          <h1>Dashboard</h1>
+
+          <div className="wrapper">
+          <div className="row">
+            <div className="box box-first">
+              <span className=" icon fas fa-users"></span>
+              <span>397</span>
+              <span>Usuários</span>
+            </div>
+
+            <div className="box box-second">
+              <span className=" icon fas fa-truck-loading"></span>
+              <span>43</span>
+              <span>Entregas</span>
+            </div>
+
+            <div className="box box-third">
+              <span className=" icon fas fa-check-circle"></span>
+              <span>12</span>
+              <span>Completas</span>
+            </div>
+
+            <div className="box box-fourth">
+              <span className=" icon fas fa-exclamation-triangle"></span>
+              <span>3</span>
+              <span>Alertas</span>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
