bed64903e86f7011e526db1b34885831f4365a8c
andmatsou <andmatsou@gmail.com>
2023-04-12 13:40:09 -0300

    Adicionado o index-Alterado.css

--- login/src/pages/Login/index.js
+++ login/src/pages/Login/index.js
@@ -4,14 +4,14 @@ import React, { useState, useContext } from 'react';
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 //conexão com API
-import  api from '../../config/configApi';
+import api from '../../config/configApi';
 
 import { Context } from '../../Context/AuthContext';
 
-export const Login = () =>{
+export const Login = () => {
 
     const { state } = useLocation();
- 
+
     //usado para redirecionar página
     const navegate = useNavigate();
 
@@ -33,91 +33,90 @@ export const Login = () =>{
     });
 
     //função usada para setar valores nos campos do formulario
-    const valorInput = e => setUser({...user, [e.target.name]: e.target.value});
+    const valorInput = e => setUser({ ...user, [e.target.name]: e.target.value });
 
     //função usada para enviar valores do formulario p API
     const loginSubmit = async e => {
 
         e.preventDefault();
-       // console.log(user.password);
-       setStatus({
-        loading: true
-       });
-
-       const headers = {
-        'Content-Type': 'application/json'
-       }
-       //requisição post p API
-       await api.post("/login", user, {headers})
-       .then((response) => {
-       // console.log(response)
+        // console.log(user.password);
         setStatus({
-            /*type: 'success',
-            mensagem: response.data.mensagem,*/
-            loading: false
+            loading: true
         });
-        //salva o token no localStorage do navegador
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('name', response.data.user.name);
-        localStorage.setItem('image', response.data.user.image);
-        //função chamada do contex - seta a constante [authenticated] p true
-        signIn(true);
-        //caso login com sucesso redireciona p página dashboard
-        return navegate("/dashboard");
-       }).catch((err) => {
-        if(err.response){
-          //console.log(err.response)
-          setStatus({
-            type: 'error',
-            mensagem: err.response.data.mensagem,
-            loading: false
-        });
-        }else{
-          //console.log("Erro: tente mais tarde!");
-          setStatus({
-            type: 'error',
-            mensagem: "Erro: tente mais tarde!",
-            loading: false
-        });
+
+        const headers = {
+            'Content-Type': 'application/json'
         }
-        
-       })
+        //requisição post p API
+        await api.post("/login", user, { headers })
+            .then((response) => {
+                // console.log(response)
+                setStatus({
+                    /*type: 'success',
+                    mensagem: response.data.mensagem,*/
+                    loading: false
+                });
+                //salva o token no localStorage do navegador
+                localStorage.setItem('token', response.data.token);
+                localStorage.setItem('name', response.data.user.name);
+                localStorage.setItem('image', response.data.user.image);
+                //função chamada do contex - seta a constante [authenticated] p true
+                signIn(true);
+                //caso login com sucesso redireciona p página dashboard
+                return navegate("/dashboard");
+            }).catch((err) => {
+                if (err.response) {
+                    //console.log(err.response)
+                    setStatus({
+                        type: 'error',
+                        mensagem: err.response.data.mensagem,
+                        loading: false
+                    });
+                } else {
+                    //console.log("Erro: tente mais tarde!");
+                    setStatus({
+                        type: 'error',
+                        mensagem: "Erro: tente mais tarde!",
+                        loading: false
+                    });
+                }
+
+            })
     }
 
-    return(
+    return (
         <div className="d-flex">
             <div className="container-login">
-            <div className="wrapper-login">
-            <div className="title">
-                <span>Sigap - Área Restrita</span>
-            </div>
-            
-            
-            <form onSubmit={loginSubmit} className="form-login">
+                <div className="wrapper-login">
+                    <div className="title">
+                        <span>Sigap - Área Restrita</span>
+                    </div>
 
-            {status.type === 'error' ? <p className="alert-danger">{status.mensagem}</p> : ""}
-            {status.type === 'success' ? <p className="alert-success">{status.mensagem}</p> : ""}
+                    <form onSubmit={loginSubmit} className="form-login">
 
-            {status.loading ? <p className="alert-success">Validando...</p> : ""}
+                        {status.type === 'error' ? <p className="alert-danger">{status.mensagem}</p> : ""}
+                        {status.type === 'success' ? <p className="alert-success">{status.mensagem}</p> : ""}
 
-                <div className="row">
-                <i className="fas fa-user"></i>
-                <input type="text" name="email" placeholder="Digite o e-mail" onChange={valorInput} />
+                        {status.loading ? <p className="alert-success">Validando...</p> : ""}
+
+                        <div className="row">
+                            <i className="fas fa-user"></i>
+                            <input type="text" name="email" placeholder="Digite o e-mail" onChange={valorInput} />
+                        </div>
+                        <div className="row">
+                            <i className="fas fa-lock"></i>
+                            <input type="password" name="password" placeholder="Digite a senha" autoComplete='on' onChange={valorInput} />
+                        </div>
+
+                        <div className="row button">
+                            {status.loading ? <button type="submit" className='button-login' disabled>Acessando...</button> : <button type="submit" className='button-login'>Acessar</button>}
+                        </div>
+                        <div className="signup-link">
+                            <Link to="/add-user-login" className='link-pg-login'>Cadastrar</Link>{" - "}
+                            <Link to="/recover-password" className='link-pg-login'>Esqueceu a senha?</Link>
+                        </div>
+                    </form>
                 </div>
-                <div className="row">
-                <i className="fas fa-lock"></i>
-                <input type="password" name="password" placeholder="Digite a senha" autoComplete='on' onChange={valorInput} />
-                </div>
-
-                <div className="row button">
-                {status.loading ? <button type="submit" className='button-login' disabled>Acessando...</button> : <button type="submit" className='button-login'>Acessar</button>}
-                </div> 
-                <div className="signup-link">
-                <Link to="/add-user-login" className='link-pg-login'>Cadastrar</Link>{" - "}
-                <Link to="/recover-password" className='link-pg-login'>Esqueceu a senha?</Link>
-                </div>              
-            </form>                      
-            </div>
             </div>
         </div>
     );
