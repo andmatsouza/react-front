d6fa5afb0dc4f1d6b38182d1a905f53d12f38f13
andmatsou <andmatsou@gmail.com>
2023-04-19 11:18:09 -0300

    feat: Adicionado o componente CardContent

--- login/src/pages/Home/index.js
+++ login/src/pages/Home/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-import {Login} from '../Login';
-
-//import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { Login } from '../Login';
+import { CardContent } from '../../components/CardContent';
 
 export const Home = () => { 
 
@@ -19,118 +15,7 @@ export const Home = () => {
       <div className="container-login">
         {<Login />}
         <div className="container-text">
-          <Row xs={1} md={2} className="g-4">
-            <Col>
-              <Card>
-                <Card.Img variant="top" src="/banner-card-7.png" />
-                <Card.Body>
-                  <Card.Title>SIGAP</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <Card.Img variant="top" src="/banner-card-8.png" />
-                <Card.Body>
-                  <Card.Title>SEMINÁRIO</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            {/* <Col>
-              <Card>
-                <Card.Img variant="top" src="/banner-card-5.png" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <Card.Img variant="top" src="/banner-card-6.png" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col> */}
-          </Row>
-          {/* <Card>
-            <Card.Img variant="top" src="/banner-card-2.png" />
-            <Card.Body>
-              <Card.Text>
-              Tem por missão precípua gerenciar as pesquisas na área de
-                Psicologia, as avaliações psicológicas e apoiar as atividades de
-                ensino do Departamento de Educação e Cultura do Exército (DECEx)
-              </Card.Text>
-            </Card.Body>
-          </Card> 
-
-          <Card>
-            <Card.Img variant="top" src="/banner-card-4.png" />
-            <Card.Body>
-              <Card.Text>
-              Tem por missão precípua gerenciar as pesquisas na área de
-                Psicologia, as avaliações psicológicas e apoiar as atividades de
-                ensino do Departamento de Educação e Cultura do Exército (DECEx)
-              </Card.Text>
-            </Card.Body>
-          </Card>  */}
-
-          {/* <Card style={{ width: "22rem" }}>
-            <Card.Img variant="top" src="/banner-site-psc.png" />
-            <Card.Body>
-              <Card.Title>CPAEx - MISSÃO</Card.Title>
-              <Card.Text>
-                Tem por missão precípua gerenciar as pesquisas na área de
-                Psicologia, as avaliações psicológicas e apoiar as atividades de
-                ensino do Departamento de Educação e Cultura do Exército (DECEx)
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "22rem" }}>
-            <Card.Img variant="top" src="/banner-site.png" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Tem por missão precípua gerenciar as pesquisas na área de
-                Psicologia, as avaliações psicológicas e apoiar as atividades de
-                ensino do Departamento de Educação e Cultura do Exército (DECEx)
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-          <Card style={{ width: "22rem" }}>
-            <Card.Img variant="top" src="/banner-site-psc.png" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Tem por missão precípua gerenciar as pesquisas na área de
-                Psicologia, as avaliações psicológicas e apoiar as atividades de
-                ensino do Departamento de Educação e Cultura do Exército (DECEx)
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card> */}
+          {<CardContent />}          
         </div>
       </div>
     </div>
